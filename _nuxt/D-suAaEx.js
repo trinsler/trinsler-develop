014ccd09428ae1cf3b08a0c3f9875aeb134d906a
Nielshen <Nielshen@users.noreply.github.com>
2025-09-20 11:43:24 +0000

    deploy: trinsler/landingPages@01edbb429c83880bf0b870d42d46ec142937f749

--- _nuxt/D-suAaEx.js
+++
@@ -1,1 +0,0 @@
-import{p as s,q as a,s as r,x as u,y as o}from"#entry";function i(e){const n=e||a();return n.ssrContext?.head||n.runWithContext(()=>{if(r()){const t=u(o);if(!t)throw new Error("[nuxt] [unhead] Missing Unhead instance.");return t}})}function d(e,n={}){const t=i(n.nuxt);return s(e,{head:t,...n})}export{d as u};
